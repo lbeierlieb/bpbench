08b723143682c32e2270402717268a50dde8c8a1
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2024-12-19 00:16:48 +0100

    bpbench linux: add page read bench function

--- src/bpbench_linux.c
+++ src/bpbench_linux.c
@@ -125,6 +125,47 @@ void bench_read_qword(void *addr) {
   printf("max time    : %ld\n", max);
 }
 
+long time_read_page(void *addr) {
+  struct timespec start, end;
+  long val;
+  long volatile *ptr = addr;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  for (int i = 0; i < PAGE_SIZE / sizeof(long); i++) {
+    val = ptr[i];
+  }
+  clock_gettime(CLOCK_MONOTONIC, &end);
+  long seconds = end.tv_sec - start.tv_sec;
+  long nanoseconds = end.tv_nsec - start.tv_nsec;
+  long elapsed = seconds * 1e9 + nanoseconds;
+
+  return elapsed;
+}
+
+void bench_read_page(void *addr) {
+  unsigned int repetitions = 100000;
+  long times[repetitions];
+  for (int i = 0; i < repetitions; i++) {
+    times[i] = time_read_page(addr);
+  }
+  long sum = times[0];
+  long min = times[0];
+  long max = times[0];
+  for (int i = 1; i < repetitions; i++) {
+    long time = times[i];
+    sum += time;
+    if (time < min) {
+      min = time;
+    } else if (time > max) {
+      max = time;
+    }
+  }
+  double avg = (double)sum / repetitions;
+  printf("repetitions : %u\n", repetitions);
+  printf("average time: %.2f\n", avg);
+  printf("min time    : %ld\n", min);
+  printf("max time    : %ld\n", max);
+}
+
 int main() {
   check_system_page_size();
 
@@ -143,6 +184,9 @@ int main() {
 
   printf("Reading from page...\n");
   bench_read_qword(breakpoint_location);
+
+  printf("Reading whole page...\n");
+  bench_read_page(exec_mem);
 
   // Free the memory
   if (munmap(exec_mem, PAGE_SIZE) != 0) {
